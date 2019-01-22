aa102b319031f2fdbf7aab088f07bb1a4a34de05
Johannes Berthel <j.berthel@codeblick.de>
2019-01-22 12:04:05 +0100

    added newer and older shopware versions

--- build.sh
+++ build.sh
@@ -8,4 +8,26 @@ build() {
         -q
 }
 
+build 5.3.0
+build 5.3.1
+build 5.3.2
+build 5.3.3
+build 5.3.4
+build 5.3.5
+build 5.3.6
+build 5.3.7
+
+build 5.4.0
+build 5.4.1
+build 5.4.2
+build 5.4.3
+build 5.4.4
+build 5.4.5
+build 5.4.6
+
+build 5.5.0
+build 5.5.1
+build 5.5.2
+build 5.5.3
 build 5.5.4
+build 5.5.5
--- push.sh
+++ push.sh
@@ -7,4 +7,26 @@ push() {
 
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
+push 5.3.0
+push 5.3.1
+push 5.3.2
+push 5.3.3
+push 5.3.4
+push 5.3.5
+push 5.3.6
+push 5.3.7
+
+push 5.4.0
+push 5.4.1
+push 5.4.2
+push 5.4.3
+push 5.4.4
+push 5.4.5
+push 5.4.6
+
+push 5.5.0
+push 5.5.1
+push 5.5.2
+push 5.5.3
 push 5.5.4
+push 5.5.5
