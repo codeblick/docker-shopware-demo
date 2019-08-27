e29c212ad0987cf96b31b57af1710140fffd12b1
j.berthel <j.berthel@codeblick.de>
2019-08-27 15:50:54 +0200

    added newer shopware versions, removed old ones

--- push.sh
+++ push.sh
@@ -7,15 +7,6 @@ push() {
 
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
-push 5.3.0
-push 5.3.1
-push 5.3.2
-push 5.3.3
-push 5.3.4
-push 5.3.5
-push 5.3.6
-push 5.3.7
-
 push 5.4.0
 push 5.4.1
 push 5.4.2
@@ -33,3 +24,7 @@ push 5.5.5
 push 5.5.6
 push 5.5.7
 push 5.5.8
+push 5.5.9
+push 5.5.10
+
+push 5.6.0
